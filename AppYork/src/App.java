e84e5e23eb158e144fcef93924b1d56a6118e26d
YorkTH12 <bird0948979449@gmail.com>
2023-11-01 14:03:56 +0700

    first commit

---
+++ AppYork/src/App.java
@@ -0,0 +1,64 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+ // String myname = "Itthisak";
+        // System.out.println("Name" + myname);
+
+        // System.out.print("Enter your number 1 : ");
+        // Scanner sc1 = new Scanner(System.in);
+        // int x = sc1.nextInt();
+
+        // System.out.print("Enter your number 2 : ");
+        // Scanner sc2 =new Scanner(System.in);
+        // int y = sc2.nextInt();
+
+        // sc1.close();
+        // sc2.close();
+
+        // System.out.println("\n x + y" + "=" + (x+y));
+ // System.out.print(">>Enter your number A : ");
+        // Scanner sc1 = new Scanner(System.in);
+        // int a = sc1.nextInt();
+        // System.out.print(">>Enter your number B : ");
+        // Scanner sc2 = new Scanner(System.in);
+        // int b = sc2.nextInt();
+        // System.out.println("----------------------");
+        // System.out.println("**RESULT**");
+        // System.out.println("A+B = "+(a+b));
+        // System.out.println("A-B = "+(a-b));
+        // System.out.println("A*B = "+(a*b));
+        // System.out.println("A/B = "+(a/b));
+
+        // int score = 55;
+        // System.out.println((score>=50)?"pass":"fail");
+
+        // System.out.print("Input 1-7 : ");
+        // Scanner sc1 = new Scanner(System.in);
+        // int day = sc1.nextInt();
+
+        // switch (day) {
+        //     case 1:
+        //         System.out.println("Monday");
+        //         break;
+        //     case 2:
+        //         System.out.println("Tuesday");
+        //         break;
+        //     case 3:
+        //         System.out.println("Wednesday");
+        //         break;
+        //     case 4:
+        //         System.out.println("Thursday");
+        //         break;
+        //     case 5:
+        //         System.out.println("Friday");
+        //         break;
+        //     case 6:
+        //         System.out.println("Saturday");
+        //         break;
+        //     case 7:
+        //         System.out.println("Sunday");
+        //         break;
+        //     default:
+        //         System.out.println("Error");
+        //         break;
+    }
+}
---
+++ AppYork/src/StundentGrade.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+public class StundentGrade {
+    public static void main(String[] args) {
+    //     System.out.println("Nakhon Pathom Vocational College");
+    //     System.out.println("Name : Itthisak Sasriot");
+    //     System.out.println("Nickname : York");
+    //     System.out.println(("Student ID : 65309010009"));
+    //     System.out.println("--------------------------------");
+
+       
+    //     System.out.print("Enter your GPA : ");
+    //     Scanner sc1 = new Scanner(System.in);
+    //     int x = sc1.nextInt();
+    //     System.out.println("Result GPA : " + x);
+    //     char y;
+    //    if (x>=80) {
+    //     y = 'A';
+    //    }else if(x>=70){
+    //     y = 'B';
+    //    }else if(x>=60){
+    //     y = 'C';
+    //    }else if(x>=50){
+    //     y = 'D';
+    //    }else if(x<50){
+    //     y = 'F';
+    //    }else{
+    //     y = '?';
+    //    }
+    //    System.out.println("Grade : "+y);
+
+    //     System.out.println("-----Menu-----");
+    //     System.out.println("1 : Spaghetti");
+    //     System.out.println("2 : Green curry");
+    //     System.out.println("3 : Hamburger");
+    //     System.out.println("4 : Pizza");
+    //     System.out.println("5 : Sushi");
+    //     System.out.print("Please enter number for select menu : ");
+    //     Scanner sc1 = new Scanner(System.in);
+    //     int x = sc1.nextInt();
+    //     switch (x) {
+    //         case 1:
+    //             System.out.println("You select : Spaghetti");
+    //             break;
+    //         case 2:
+    //             System.out.println("You select : Green curry");
+    //             break;    
+    //         case 3:
+    //             System.out.println("You select : Hamburger");
+    //             break;
+    //         case 4:
+    //             System.out.println("You select : Pizza");
+    //             break;
+    //         case 5:
+    //             System.out.println("You select : Sushi");
+    //             break;     
+    //         default:
+    //             System.out.println("Please again");
+    //             break;
+    //     }
+
+    int num=0;
+    for(int i=1;i<=10;i++){
+        num += i;
+    }
+    System.out.println("Summary : "+ num);
+     }
+}
